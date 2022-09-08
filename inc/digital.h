55cbedcf708462519de7157d8be139109a163814
German Moyano <germoyanoh1@gmail.com>
2022-09-08 20:47:22 -0300

    Primera version con los archivos para la HAL creados y virgenes

---
+++ inc/digital.h
@@ -0,0 +1,66 @@
+/* Copyright 2022, Laboratorio de Microprocesadores 
+ * Facultad de Ciencias Exactas y Tecnología 
+ * Universidad Nacional de Tucuman
+ * http://www.microprocesadores.unt.edu.ar/
+ * Copyright 2022, Esteban Volentini <evolentini@herrera.unt.edu.ar>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef DIGITAL_H   /*! @cond    */
+#define DIGITAL_H   /*! @endcond */
+
+/** \brief Definicion de Entradas y salidas digitales
+ ** 
+ ** \addtogroup hal HAL
+ ** \brief Capa de abstraccion de Hardware
+ ** @{ */
+
+/* === Inclusiones de archivos externos ==================================== */
+
+/* === Cabecera C++ ======================================================== */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* === Definicion y Macros publicos ======================================== */
+
+/* == Declaraciones de tipos de datos publicos ============================= */
+
+/* === Declaraciones de variables publicas ================================= */
+
+/* === Declaraciones de funciones publicas ================================= */
+
+/* === Ciere de documentacion ============================================== */
+#ifdef __cplusplus
+}
+#endif
+
+/** @} Final de la definición del modulo para doxygen */
+
+#endif   /* DIGITAL_H */
